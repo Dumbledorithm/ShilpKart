36ae037275e465e4d9ec899d31b026d59257865d
Dumbledorithm <mishrashardendu3@gmail.com>
2025-09-01 13:54:23 +0530

    config

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -8,7 +8,7 @@ const configurePassport = (passport) => {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback',
+        callbackURL: '/api/auth/google/callback',
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
